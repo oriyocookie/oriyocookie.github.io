50f9252d94d776b69512a2793c84eed65ecd0daf
Oriyo Chakma <oriyochakma@gmail.com>
2019-06-08 14:54:24 +0600

    js

--- walkthrough/OverthewireNarnia/scripts.js
+++ walkthrough/OverthewireNarnia/scripts.js
@@ -6,7 +6,8 @@ var modal = document.getElementById("myModal");
 var img = document.getElementById("myImg");
 var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
-img.onclick = function(){
+img.onclick = function()
+{
     modal.style.display = "block";
     modalImg.src = this.src;
     captionText.innerHTML = this.alt;
