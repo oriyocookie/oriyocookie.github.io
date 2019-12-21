794b57b2ee981a7bcd4f745dae9ff6c9ad674bb5
oriyocookie <oriyocookie@github.com>
2019-12-21 02:04:29 -0600

    Updated placeholder part of site

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,11 +2,11 @@
 M.AutoInit();
 
 function watashiHover(element) {
-	element.setAttribute('src', 'images/oportunitygrabbed.jpg');
+	element.setAttribute('src', '/images/oportunitygrabbed.jpg');
 }
 
 function watashiUnhover(element) {
-	element.setAttribute("src", "images/oportunity.png");
+	element.setAttribute("src", "/images/oportunity.png");
 }
 
  var elem = document.querySelector('.sidenav');
